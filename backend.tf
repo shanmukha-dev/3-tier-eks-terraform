d780717ea02770c1da7df8fbae0638435a2b542f
Mauliksha <shahmaulik51995@gmail.com>
2024-07-12 13:57:45 +0000

    update backend.tf file

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hypha-test-bucket"
+    bucket = "eks-bucket-12-07-2024"
     key    = "k8s_webapp_tf_state_file.tfstate"
     region = "ap-south-1"
   }
