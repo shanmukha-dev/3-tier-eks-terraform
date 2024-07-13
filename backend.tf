d1e3141b79fecd11b9827781a3e5d37664c8b1dd
Mauliksha <shahmaulik51995@gmail.com>
2024-07-13 02:00:08 +0000

    backend update

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eks-bucket-12-07-2024"
+    bucket = "counter-strike-bucket"
     key    = "k8s_webapp_tf_state_file.tfstate"
     region = "ap-south-1"
   }
