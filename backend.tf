2b5a3190e42c09328b216b359d0865fac1fff898
Vijay-hypha <vijay.hypha098@gmail.com>
2024-08-04 11:59:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hypha-test-bucket-123"
+    bucket = "hypha-test-bucket-1234"
     key    = "k8s_webapp_tf_state_file.tfstate"
     region = "us-east-1"
   }
