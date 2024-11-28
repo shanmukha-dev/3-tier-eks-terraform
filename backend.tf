f4b194a7d8e51b35651898ff32e965bbd325708d
shanmukha-dev <shanmukha.hypha@gmail.com>
2024-11-28 10:44:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hypha-test-bucket"
+    bucket = "hypha-tf-state-bucket"
     key    = "k8s_webapp_tf_state_file.tfstate"
     region = "ap-south-1"
   }
